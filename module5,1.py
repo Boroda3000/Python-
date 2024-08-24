a1408381f56b58aecf9623fa4036acfb3eb9f297
Boroda3000 <pavluk.a.m@yandex.ru>
2024-08-24 11:44:09 +0300

    домашка по Классам

---
+++ module5,1.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, name, num_of_floors):
+        self.name = name
+        self.num_of_floors = num_of_floors
+
+
+    def go_to(self, new_floor):
+        if new_floor < 1  or new_floor > self.num_of_floors:
+            print('Такого этажа не существует!')
+        else:
+            print(f'Выбранный этаж: {new_floor}')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h3 = House('Зеленая долина', 20)
+h1.go_to(5)
+h1.go_to(35)
+h1.go_to(17)
+h2.go_to(1)
+h2.go_to(10)
+h3.go_to(13)
+h3.go_to(0)
