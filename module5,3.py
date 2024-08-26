a6a9c3a5eb176a16f1216f4349d7cc0e13eb108a
Boroda3000 <pavluk.a.m@yandex.ru>
2024-08-26 11:26:19 +0300

    на проверку 2.0

--- module5,3.py
+++ module5,3.py
@@ -71,7 +71,7 @@ class House:
         if isinstance(other, (int, float)):
             return self.num_of_floors + other
         else:
-            raise TypeError('Невозможно сложить объект "House" с другим типом')
+            print('Невозможно сложить объект "House" с другим типом')
 
 
     def __radd__(self, other):
@@ -86,7 +86,7 @@ class House:
             self.num_of_floors += other
             return self
         else:
-            raise TypeError('Невозможно сложить объект "House" с другим типом')
+            print('Невозможно сложить объект "House" с другим типом')
 
 
     def go_to(self, new_floor):
@@ -113,3 +113,24 @@ x = h1 + h2
 print(x)
 h1 += h3
 print(h1)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
