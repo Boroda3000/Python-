5a338ae82cf24d47d4a70fcf2f5636b903141c35
Boroda3000 <pavluk.a.m@yandex.ru>
2024-10-13 13:44:18 +0300

    на проверку

---
+++ module_9/module_9,6.py
@@ -0,0 +1,12 @@
+def all_variants(text):
+
+    for i in range(len(text) + 1):
+        for j in range(i, len(text) + 1):
+            if i != j:
+                yield text[i:j]
+
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
