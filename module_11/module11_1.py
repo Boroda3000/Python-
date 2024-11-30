14ac0d6b607696d6e655fb74a0d200ee80299f60
Boroda3000 <pavluk.a.m@yandex.ru>
2024-11-30 22:15:38 +0300

    на проверку

---
+++ module_11/module11_1.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+
+
+github_token = "Токен авторизации GitHub"  
+
+reposit_name = "Репозиторий на GitHub"
+url = f"https://api.github.com/repos/{reposit_name}"
+headers = {
+    "Authorization": f"token {github_token}",
+    "Accept": "application/vnd.github+json"
+}
+
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    reposit_data = response.json()
+    reposit_df = pd.DataFrame([reposit_data])
+    print(reposit_df)
+
+except requests.exceptions.RequestException as e:
+    print(f"Произошла ошибка: {e}")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+
+# выполняем запрос к API сайта, проверяем наличие ошибок, преобразуем ответ с сайта в формате JSON в Data Frame и выводим. 
