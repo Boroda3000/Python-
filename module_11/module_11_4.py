65084c72f189457268e12663c4a3db1e1dd6d588
Boroda3000 <pavluk.a.m@yandex.ru>
2024-12-12 21:19:55 +0300

    на проверку

--- module_11/module_11_4.py
+++ module_11/module_11_4.py
@@ -16,16 +16,20 @@ class TournamentTest(unittest.TestCase):
     
     @classmethod
     def tearDownClass(cls):
-        print(cls.all_results)
-
+        print("Результаты всех турниров:")
+        for tournament_name, results in cls.all_results.items():
+            print(f"{tournament_name}:")
+            for place, runner in results.items():
+                print(f"{place} место: {runner.name}")
     def tearDown(self):
         print('Результат забега:')
-        for i in range(self.count_participants):
-            print(f'{i + 1}: {self.tour_result[i + 1].name}')
+        for i in range(len(self.tour_result)):
+            print(f'{i + 1} : {self.tour_result[i+1]}')
        
     def test_Tournament1(self):
         test_tour = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
         self.tour_result = test_tour.start()
+        self.count_participants = len(test_tour.participants)
         try:
             largest_key = max(self.tour_result)
             self.assertEqual(self.tour_result[largest_key].name, 'Ник')
@@ -34,8 +38,9 @@ class TournamentTest(unittest.TestCase):
         self.all_results[f'Турнир №1'] = self.tour_result
 
     def test_Tournament2(self):
-        test_tour = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
+        test_tour = runner_and_tournament.Tournament(90, self.runner_2, self.runner_3)
         self.tour_result = test_tour.start()
+        self.count_participants = len(test_tour.participants)
         try:
             largest_key = max(self.tour_result)
             self.assertEqual(self.tour_result[largest_key].name, 'Ник')
@@ -44,14 +49,15 @@ class TournamentTest(unittest.TestCase):
         self.all_results[f'Турнир №2'] = self.tour_result
 
     def test_Tournament3(self):
-        test_tour = runner_and_tournament.Tournament(90, self.runner_1, self.runner_3)
+        test_tour = runner_and_tournament.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
         self.tour_result = test_tour.start()
+        self.count_participants = len(test_tour.participants)
         try:
             largest_key = max(self.tour_result)
             self.assertEqual(self.tour_result[largest_key].name, 'Ник')
         except (ValueError, KeyError):
             self.fail("Ошибка при доступе к результатам")
-        self.all_results[f'Турнир №1'] = self.tour_result
+        self.all_results[f'Турнир №3'] = self.tour_result
 
 
 
